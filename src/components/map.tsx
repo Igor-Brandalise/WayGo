10c6766753e21953d6ad0373f68c5d9167ba1a4c
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-24 22:16:46 -0300

    feat: useEffect para pegar latitude e longitude

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,24 +1,44 @@
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 
 function MyComponent() {
   const map = useMap();
-  console.log("map center:", map.getCenter());
+
+  const [center, setCenter] = useState([55.5, 30.9]);
+
+  useEffect(() => {
+    const handleMove = () => {
+      const novoCentro = map.getCenter();
+      setCenter([novoCentro.lat, novoCentro.lng]);
+
+      console.log(novoCentro);
+    };
+
+    map.on("moveend", handleMove);
+    return () => {
+      map.off("moveend", handleMove);
+    };
+  }, [map]);
+
   return null;
 }
 
 export function Map() {
   return (
-    <MapContainer
-      center={[50.5, 30.5]}
-      zoom={13}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
+    <div>
+      <MapContainer
+        center={[50.5, 30.5]}
+        zoom={13}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
 
-      <MyComponent />
-    </MapContainer>
+        <MyComponent />
+      </MapContainer>
+    
+    </div>
   );
 }
