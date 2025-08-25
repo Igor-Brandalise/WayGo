9fa18a4127817e36bdee67fa13cb782b73f16f0d
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-24 23:04:00 -0300

    feat: input de pesquisa

---
+++ src/components/input.tsx
@@ -0,0 +1,13 @@
+export function Pesquisa() {
+  return (
+    <div>
+      <input
+        type="search"
+        name=""
+        id=""
+        className="w-2xs rounded-[.7rem] p-2  h-6 bg-white ml-5 z-5"
+        placeholder="Digite um local"
+      />
+    </div>
+  );
+}
