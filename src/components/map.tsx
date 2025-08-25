03514227cdd8474270c04ae26c85fac57fab061f
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-24 23:03:20 -0300

    feat: importando o componente de pesquisa e ajustando o layout

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { useEffect, useState } from "react";
+import { Pesquisa } from "./input";
 
 function MyComponent() {
   const map = useMap();
@@ -25,20 +26,27 @@ function MyComponent() {
 
 export function Map() {
   return (
-    <div>
+    <div className="relative h-screen w-full">
       <MapContainer
         center={[50.5, 30.5]}
         zoom={13}
-        style={{ height: "100vh", width: "100%" }}
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "relative",
+          zIndex: 0,
+        }}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; OpenStreetMap contributors"
         />
-
         <MyComponent />
       </MapContainer>
-    
+
+      <div className="absolute top-4 left-12 z-50 ">
+        <Pesquisa />
+      </div>
     </div>
   );
 }
